d34f5e068197fcdd6c8d14d7e050cd18eb235f8a
gatsbimantico <costascarrera.cesar@gmail.com>
2024-08-17 09:25:56 +0100

    Speeding up build by removing linting and typechecking, downgrading docker-compose

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   transpilePackages: ['duckdb'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack: (config) => {
     config.externals = [...config.externals, 'duckdb'];
     config.resolve = { ...config.resolve, alias: { ...config.resolve.alias,
