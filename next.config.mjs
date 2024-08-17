9e990ebcf17312e14b861295bdff8ddb31345bb8
gatsbimantico <costascarrera.cesar@gmail.com>
2024-08-17 06:07:04 +0100

    Attempt to deploy from Github

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   transpilePackages: ['duckdb'],
   webpack: (config) => {
     config.externals = [...config.externals, 'duckdb'];
+    config.resolve = { ...config.resolve, alias: { ...config.resolve.alias,
+      "@/*": ["./src/*"]
+    }}
     return config;
   },
 };
