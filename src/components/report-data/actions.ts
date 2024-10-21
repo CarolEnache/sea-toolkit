34be63dba8513f8b608ce5a22f387b51a4f78551
Cesar Costas <costascarrera.cesar@gmail.com>
2024-10-21 06:44:54 +0000

    Report not working, public server setup finished

--- src/components/report-data/actions.ts
+++ src/components/report-data/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import { reportService } from "@/server/services";
+import { reportService } from "@/dummy-server/services";
 
 export async function getReportDataAction(id: string) {
-    return reportService.generateReport(id);
-  }
+  return reportService.generateReport(id);
+}
